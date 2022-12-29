67d4b0180ed4ebbf95936bb037fd431acf8e0b7f
chulyeon <guinness987@gmail.com>
2022-12-29 14:53:54 +0900

    feat: home header add

---
+++ Daytour/View/HomeHeaderStackView.swift
@@ -0,0 +1,59 @@
+import UIKit
+
+class HomeHeaderStackView: UIStackView {
+	//MARK: - Properties
+	let titleStackView: UIStackView = {
+		let stack = UIStackView()
+		stack.translatesAutoresizingMaskIntoConstraints = false
+		stack.distribution = .equalSpacing
+		stack.axis = .vertical
+		return stack
+	}()
+	let titleLabel: UILabel = {
+		let label = UILabel()
+		label.translatesAutoresizingMaskIntoConstraints = false
+		label.text = "Welcome!"
+		label.font = UIFont.monospacedSystemFont(ofSize: 18, weight: .bold)
+		label.layer.opacity = 0.5
+		label.textColor = UIColor(named: "Title")
+		return label
+	}()
+	let nicknameLabel: UILabel = {
+		let label = UILabel()
+		label.translatesAutoresizingMaskIntoConstraints = false
+		label.text = "user"
+		label.font = UIFont.monospacedSystemFont(ofSize: 25, weight: .bold)
+		label.textColor = UIColor(named: "Primary")
+		return label
+	}()
+	let profileImgButton: UIButton = {
+		let btn = UIButton(type: .system)
+		btn.translatesAutoresizingMaskIntoConstraints = false
+		btn.setImage(UIImage(systemName: "person.crop.circle"), for: .normal)
+		btn.imageView?.contentMode = .scaleAspectFit
+		btn.tintColor = UIColor(named: "Secondary")
+		btn.setPreferredSymbolConfiguration(.init(pointSize: 30, weight: .regular, scale: .default), forImageIn: .normal)
+		return btn
+	}()
+
+	//MARK: - Lifecycle
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+
+		translatesAutoresizingMaskIntoConstraints = false
+		distribution = .equalCentering
+		isLayoutMarginsRelativeArrangement = true
+		layoutMargins = .init(top: 15, left: 20, bottom: 0, right: 20)
+		
+		[titleLabel, nicknameLabel].forEach { view in
+			titleStackView.addArrangedSubview(view)
+		}
+		[titleStackView, profileImgButton].forEach { view in
+			addArrangedSubview(view)
+		}
+	}
+
+	required init(coder: NSCoder) {
+		super.init(coder: coder)
+	}
+}
