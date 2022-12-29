6e8fef8367f5b03d9865faa0c855fb54c47cf6cf
chulyeon <guinness987@gmail.com>
2022-12-29 10:51:30 +0900

    feat: 에러 알림창 메소드 공통화

---
+++ Daytour/Extension/ViewController+Ext.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+extension UIViewController {
+	
+	/// 에러 알림창 생성 메소드
+	/// - Parameters:
+	///   - error: error 인자
+	///   - title: 알림창 타이틀
+	///   - message: 알림창 내용
+	///   - okTitle: 알림창 버튼 타이틀
+	func generateErrorAlert(error: Any, title: String, message: String, okTitle: String) {
+		print("Error Log In with \(error)")
+		let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+		let okAction = UIAlertAction(title: okTitle, style: .destructive)
+		alert.addAction(okAction)
+		self.present(alert, animated: true)
+	}
+}
